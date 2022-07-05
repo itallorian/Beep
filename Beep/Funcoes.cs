6d0ccb6e812b248333755186df33dde24861dcbf
Ítallo Rian <me@itallorian.com>
2022-07-05 16:02:03 -0300

    Add project files.

---
+++ Beep/Funcoes.cs
@@ -0,0 +1,59 @@
+﻿namespace Beep
+{
+    public class Funcoes
+    {
+        public int EscolherFrequencia()
+        {
+            int frequencia;
+
+            Console.WriteLine("Escolha a frequência do Beep (hertz): ");
+            string frequenciaInput = Console.ReadLine();
+
+            if (Int32.TryParse(frequenciaInput, out frequencia) == false)
+            {
+                return 800;
+            }
+
+            return frequencia;
+        }
+
+        public int EscolherDuracao()
+        {
+            int duracao;
+
+            Console.WriteLine("Escolha a duração do Beep (milisegundos): ");
+            string duracaoInput = Console.ReadLine();
+
+            if(Int32.TryParse(duracaoInput, out duracao) == false)
+            {
+                return 200;
+            }
+
+            return duracao;
+        }
+
+        public void ModoConstante(int frequencia, int duracao)
+        {
+            Console.WriteLine("Pressione CTRL + C para sair...");
+
+            while (true)
+            {
+                Console.Beep(frequencia, duracao);
+            }
+        }
+
+        public void ModoToque(int frequencia, int duracao)
+        {
+            Console.WriteLine("Pressione X ou CTRL + C para sair...");
+
+            ConsoleKeyInfo informacaoTecla;
+
+            do
+            {
+                informacaoTecla = Console.ReadKey();
+                Console.Beep(frequencia, duracao);
+            }
+            while (informacaoTecla.Key != ConsoleKey.X);
+        }
+    }
+}
---
+++ Beep/Program.cs
@@ -0,0 +1,37 @@
+﻿using Beep;
+
+string versao = "1.0";
+string escolha;
+int frequencia, duracao;
+Funcoes funcao = new Funcoes();
+
+Console.WriteLine("=======================================");
+Console.WriteLine("Beep: O programa que bipa! - Versão " + versao);
+Console.WriteLine("=======================================\n");
+
+Console.WriteLine("Escolha o modo do programa:");
+Console.WriteLine("---------------------------");
+Console.WriteLine("(1) Modo Toque - Bipa quando tecla for pressionada");
+Console.WriteLine("(2) Modo Constante - Bipa constantemente até o programa ser fechado\n");
+
+Console.Write("Escolha o modo (qualquer outra opção para sair): ");
+escolha = Console.ReadLine();
+
+switch (escolha)
+{
+    case "1":
+        frequencia = funcao.EscolherFrequencia();
+        duracao = funcao.EscolherDuracao();
+
+        funcao.ModoToque(frequencia, duracao);
+        break;
+    case "2":
+        frequencia = funcao.EscolherFrequencia();
+        duracao = funcao.EscolherDuracao();
+
+        funcao.ModoConstante(frequencia, duracao);
+        break;
+    default:
+        Console.WriteLine("Saindo...");
+        break;
+}
